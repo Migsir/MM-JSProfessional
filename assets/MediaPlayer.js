89cd74a6d0fdc30a1e29e18dcd2f81909e7ee86f
Josemiguel Mena <josemiguel_mena@jabil.com>
2020-06-09 19:37:07 -0500

    The Autoplay plugin was integrated

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,6 +1,15 @@
 function MediaPlayer(config)
 {
-    this.media = config.el
+    this.media = config.el;
+    this.plugins = config.plugins || [];
+
+    this._initPlugins();
+}
+
+MediaPlayer.prototype._initPlugins = function(){    
+    this.plugins.forEach(plugin => {
+        plugin.Run(this);
+    });
 }
 
 MediaPlayer.prototype.play = function()
@@ -13,9 +22,23 @@ MediaPlayer.prototype.pause = function()
     this.media.pause();
 }
 
+MediaPlayer.prototype.mute = function()
+{
+    this.media.muted = true;
+}
+
+MediaPlayer.prototype.unmute = function()
+{
+    this.media.muted = false;
+}
+
 MediaPlayer.prototype.togglePlay = function()
 {
     this.media.paused ? this.media.play() : this.media.pause();
+}
+
+MediaPlayer.prototype.toggleMute = function(){
+    this.media.muted = this.media.muted ? false : true;
 }
 
 export default MediaPlayer;
